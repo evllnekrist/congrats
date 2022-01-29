2ee336e55caa671f7dad2157a58a48b7860f891b
Evelline Kristiani <evelline.kristiani@kawanlamaretail.com>
2022-01-29 16:46:52 +0700

    done thumbnail [2]

--- resources/views/_invitation/_wedding/_include/head-1.blade.php
+++ resources/views/_invitation/_wedding/_include/head-1.blade.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Digital Wedding Invitation by beritabaik.co">
+<!-- <meta name="description" content="Digital Wedding Invitation by beritabaik.co"> -->
 <meta name="keywords" content="digital, wedding, invitation, beritabaik.co">
 <meta name="author" content="beritabaik.co">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<title>{{ $thumbnail_icon.$event }}</title>
+<title>{{ $event }}</title>
 <link rel="icon" href="{{@$thumbnail_icon?asset($thumbnail_icon):asset('asset-main/images/logo/logo-gold.png')}}" type="image/png">
