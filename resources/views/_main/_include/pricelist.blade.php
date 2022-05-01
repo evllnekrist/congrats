bcc149f6b03318c87c4b1ab247a95878a03c0026
t.quiko <t.quiko@bumi.ai>
2022-05-01 19:02:23 +0700

    Change pricelist

--- resources/views/_main/_include/pricelist.blade.php
+++ resources/views/_main/_include/pricelist.blade.php
@@ -1,220 +1,106 @@
-<section    class="gla_section gla_image_bck gla_wht_txt gla_fixed" data-stellar-background-ratio="0.2" 
+<section class="gla_section gla_image_bck gla_wht_txt gla_fixed" data-stellar-background-ratio="0.2" 
             data-image="{{asset('asset-main/images/bg/bride-1.png')}}" id="pricelist">    
-    <div class="gla_over" data-color="#282828" data-opacity="0.2"></div>
+    <div class="gla_over" data-color="#000" data-opacity="0.4"></div>
     <div class="container text-center">
-        <h2 class="n-font b-box-white" data-toggle="tooltip" data-placement="bottom">Our Services</h2>
+        <h2 class="n-font" data-toggle="tooltip" data-placement="bottom">Our Services</h2>
         <!-- #1::begin -->
         <h4>1. Personalized Invitation</h4>
-        <p class="highlight-2">Kamu tidak harus bayar mahal untuk dapatkan layanan premium yang tidak pasaran. Semua undangan yang dibuat dipersonalisasi berdasarkan permintaan client.</p>
-        <div class="gla_icon_boxes row" style="margin-top:-30px">
-            
+        <h5 class="gla_subtitle" style="margin-top:0px;">Kamu tidak harus bayar mahal untuk dapatkan layanan premium yang tidak pasaran. Semua undangan yang dibuat dipersonalisasi berdasarkan permintaan client.</h5>
+        <div class="gla_icon_boxes row">
+                    
             <!-- animation -->
-            <div data-animation="animation_blocks" data-bottom="@class:noactive" data--100-bottom="@class:active">
+            <div data-animation="animation_blocks" data-bottom="@class:noactive" data--100-bottom="@class:active" class="active" style="">
                 
                 <!-- item -->
-                <div class="gla_icon_box col-md-3 col-sm-6">
-                    <i class="flaticon-024-rose"></i>
+                <div class="gla_icon_box col-md-3 col-sm-6" style="transition-delay: 0s;">
                     <div class="gla_icon_box_content">
                         <h4><b>Budget</b></h4>
-                        <button type="button" class="btn btn-dark link_wa">Rp. 240.000,-</button>
-                        <br><br>
-                        <table class="table table-hover text-left">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Feature</th>
-                                </tr>
-                            </thead>
-                            <tbody class="tbody-white">
-                                <tr>
-                                    <th scope="row">1</th><td>Tujuan undangan <b>unlimited</b></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th><td>Max <b>8</b> foto</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th><td>Musik pilihan (bisa request)</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">4</th><td>Kolom ucapan (guest book)</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">5</th><td>Detail event</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">6</th><td>Navigasi peta</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">7</th><td>Link sosial media</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">8</th><td>Quotes</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">9</th><td>Profil groom & bride</td>
-                                </tr>
-                            </tbody>
-                        </table>
                     </div>
-                    
+                    <div class="gla_card">
+                        <span class="gla_news_img" style="height:0px">  
+                        </span>
+                        <span class="gla_news_title">Rp. 240.000,-</span>
+                        <ul>
+                            <li>Tujuan undangan unlimited</li>
+                            <li>Max 8 foto</li>
+                            <li>Musik pilihan (bisa request)</li>
+                            <li>Kolom ucapan (guest book)</li>
+                            <li>Detail event</li>
+                            <li>Navigasi peta</li>
+                            <li>Link sosial media</li>
+                            <li>Quotes</li>
+                            <li>Profil groom & bride</li>
+                        </ul>
+                    </div>
                 </div>
 
                 <!-- item -->
-                <div class="gla_icon_box col-md-3 col-sm-6">
-                    <i class="flaticon-041-cake"></i>
+                <div class="gla_icon_box col-md-3 col-sm-6" style="transition-delay: 0.1s;">
                     <div class="gla_icon_box_content">
                         <h4><b>Comfort</b></h4>
-                        <button type="button" class="btn btn-dark link_wa">Rp. 340.000,-</button>
-                        <br><br>
-                        <table class="table table-hover text-left">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Feature</th>
-                                </tr>
-                            </thead>
-                            <tbody class="tbody-white">
-                                <tr>
-                                    <th scope="row">1</th><td>Tujuan undangan <b>unlimited</b></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th><td>Max <b>20</b> foto</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th><td>Musik pilihan (bisa request)</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">4</th><td>Kolom ucapan (guest book)</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">5</th><td>Detail event</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">6</th><td>Navigasi peta</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">7</th><td>Link sosial media</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">8</th><td>Quotes</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">9</th><td>Profil groom & bride</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">10</th><td>Video gallery</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">11</th><td>Konfirmasi kehadiran (RSVP)</td>
-                                </tr>
-                            </tbody>
-                        </table>
                     </div>
-                    
+                    <div class="gla_card">
+                        <span class="gla_news_img" style="height:0px">  
+                        </span>
+                        <span class="gla_news_title">Rp. 340.000,-</span>
+                        <ul>
+                            <li>Tujuan undangan unlimited</li>
+                            <li>Max 8 foto</li>
+                            <li>Musik pilihan (bisa request)</li>
+                            <li>Kolom ucapan (guest book)</li>
+                            <li>Detail event</li>
+                            <li>Navigasi peta</li>
+                            <li>Link sosial media</li>
+                            <li>Quotes</li>
+                            <li>Profil groom & bride</li>
+                            <li>Video gallery</li>
+                            <li>Konfirmasi kehadiran (RSVP)</li>
+                        </ul>
+                    </div>
                 </div>
 
                 <!-- item -->
-                <div class="gla_icon_box col-md-3 col-sm-6">
-                    <i class="flaticon-005-camera-2"></i>
+                <div class="gla_icon_box col-md-3 col-sm-6" style="transition-delay: 0.2s;">
                     <div class="gla_icon_box_content">
                         <h4><b>First Class</b></h4>
-                        <button type="button" class="btn btn-dark link_wa">Rp. 500.000,-</button>
-                        <br><br>
-                        <table class="table table-hover text-left">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Feature</th>
-                                </tr>
-                            </thead>
-                            <tbody class="tbody-white">
-                                <tr>
-                                    <th scope="row">1</th><td>Tujuan undangan <b>unlimited</b></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th><td>Max <b>50</b> foto</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th><td>Musik pilihan (bisa request)</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">4</th><td>Kolom ucapan (guest book)</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">5</th><td>Detail event</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">6</th><td>Navigasi peta</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">7</th><td>Link sosial media</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">8</th><td>Quotes</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">9</th><td>Profil groom & bride</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">10</th><td>Video gallery</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">11</th><td>Konfirmasi kehadiran (RSVP)</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">12</th><td>Countdown timer</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">13</th><td>QRIS gift (link transfer)</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">14</th><td>Bonus prosedur Covid-19</td>
-                                </tr>
-                            </tbody>
-                        </table>
                     </div>
-                    
-                </div> 
+                    <div class="gla_card">
+                        <span class="gla_news_img" style="height:0px">  
+                        </span>
+                        <span class="gla_news_title">Rp. 500.000,-</span>
+                        <ul>
+                            <li>Tujuan undangan unlimited</li>
+                            <li>Max 8 foto</li>
+                            <li>Musik pilihan (bisa request)</li>
+                            <li>Kolom ucapan (guest book)</li>
+                            <li>Detail event</li>
+                            <li>Navigasi peta</li>
+                            <li>Link sosial media</li>
+                            <li>Quotes</li>
+                            <li>Profil groom & bride</li>
+                            <li>Video gallery</li>
+                            <li>Konfirmasi kehadiran (RSVP)</li>
+                            <li>Countdown timer</li>
+                            <li>QRIS gift (link transfer)</li>
+                            <li>Bonus prosedur Covid-19</li>
+                        </ul>
+                    </div>
+                </div>
 
-                <!-- more -->
-                <div class="gla_icon_box col-md-3 col-sm-6">
-                    <i class="flaticon-005-camera-2"></i>
+                <!-- item -->
+                <div class="gla_icon_box col-md-3 col-sm-6" style="transition-delay: 0.3s;">
                     <div class="gla_icon_box_content">
-                        <br><br><br>
-                        <button type="button" class="btn btn-dark link_wa">opsional</button>
-                        <br><br>
-                        <table class="table table-hover text-left">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Special Feature</th>
-                                </tr>
-                            </thead>
-                            <tbody class="tbody-white">
-                                <!-- <tr> -->
-                                    <!-- <td colspan="2">Subdomain URL >> <br> -->
-                                                    <!-- <small><b>groomname-bridename.beritabaik.co</b></small><br> -->
-                                                    <!-- <b>+ 60.000,-</b> -->
-                                    <!-- </td> -->
-                                <!-- </tr> -->
-                                <tr>
-                                    <td colspan="2">Prosedur Covid-19 >> <br>
-                                                    <b>+ 50.000,-</b>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2">Logika Tambahan >> <br>
-                                                    <b>+ 50.000,-</b><br>
-                                                    <small>* apa itu logika tambahan? Contohya hanya menampilkan acara resepsi untuk undangan tertentu, dsb.</small>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2">Request Lain? <br>
-                                                    <a href="" class="link_wa" target="_blank" style="color:green;font-weight:600">diskusikan disini</a><br>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <h4><b>Optional</b></h4>
+                    </div>
+                    <div class="gla_card">
+                        <span class="gla_news_img" style="height:0px">  
+                        </span>
+                        <span class="gla_news_title">Rp. 50.000,- / Feature</span>
+                        <ul>
+                            <li>Prosedur Covid-19</li>
+                            <li>Logika Tambahan<p style="margin-bottom:0px">* apa itu logika tambahan? <br>Contohya hanya menampilkan <br>acara resepsi untuk undangan tertentu, dsb.</p></li>
+                            <li>Request Lain? <p><a href="" class="link_wa" target="_blank" style="width:auto;height:auto;display:inline;position:static;font-weight:600;color:#666">diskusikan disini</a><br></p></li>
+                        </ul>
                     </div>
                     
                 </div> 
@@ -223,48 +109,32 @@
             <!-- anmation end -->
 
         </div>
-        <table class="table table-hover text-left">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Syarat & Ketentuan</th>
-                </tr>
-            </thead>
-            <tbody class="tbody-white">
-                <tr>
-                    <th scope="row">1</th><td>Pilih warna dasar (max 3) dan sediakan foto/video. Beritabaik.co tidak melayani edit foto/video</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th><td>Tulis detail acara (nama acara,nama tempat dan waktu). Sertakan navigasi google map pada detail</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th><td>Jika foto/video perlu ditulis waktu, tempat dan keterangan (timeline), dapat juga disertakan</td>
-                </tr>
-                <tr>
-                    <th scope="row">4</th><td>Order/diskusi dapat dilakukan <a href="" class="link_wa" target="_blank" style="color:green;font-weight:600">disini</a></td>
-                </tr>
-                <tr>
-                    <th scope="row">5</th><td>Pengerjaan 3-5 hari</td>
-                </tr>
-                <tr>
-                    <th scope="row">6</th><td>Jika dibutuhkan udangan yang telah selesai dapat direvisi (max 3 kali)</td>
-                </tr>
-                <tr>
-                    <th scope="row">7</th><td>Sebar berita baikmu ke teman dan saudara hingga jumlah tak terhingga!</td>
-                </tr>
-                <tr>
-                    <th scope="row">8</th><td>30 hari setelahnya web invitation akan diarsipkan dalam bentuk image</td>
-                </tr>
-            </tbody>
-        </table>
+        
         <!-- #1::end -->
         <!-- #2::begin -->
-        <h4>2. Company Solution</h4>
-        <p><a href="{{url('/co-solution')}}" class="highlight-1">lihat lebih lengkap >></a></p>
+        <div class="row">
+            <div class="col-12" style="margin-top:30px;">
+                <h4>2. Company Solution</h4>
+                <p>
+                    <a href="{{url('/co-solution')}}">
+                        <button class="btn btn-default">Lihat lebih lengkap</button>
+                    </a>
+                </p>
+            </div>
+        </div>
         <!-- #2::end -->
         <!-- #3::begin -->
-        <h4>3. Jasa Titip</h4>
-        <p><a href="{{url('/jastip')}}" class="highlight-1">lihat lebih lengkap >></a></p>
+        <div class="row">
+            <div class="col-12">
+                <h4>3. Jasa Titip</h4>
+                <p>
+                    <a href="{{url('/jastip')}}">
+                        <button class="btn btn-default">Lihat lebih lengkap</button>
+                    </a>
+                </p>
+            </div>
+        </div>
         <!-- #3::end -->
+        
     </div>
 </section>
